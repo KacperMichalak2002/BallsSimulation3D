f8790b970bb5f3269ade055b9a190176a8408b83
KacperMichalak2002 <michalakkacper2002@gmail.com>
2025-04-21 21:21:55 +0200

    Added elastic collision between balls
    Also added transparent material for the walls

--- Assets/BallScript.cs
+++ Assets/BallScript.cs
@@ -52,14 +52,52 @@ public class BallScript : MonoBehaviour
 
     private void HandleBallCollision(Collider otherBall) // Change to elastic collison
     {
-        Vector3 wallNormal = (transform.position - otherBall.ClosestPoint(transform.position)).normalized;
+       
+        BallScript other = otherBall.GetComponent<BallScript>();
+        if (other == null)
+            return;
 
-        Vector3 reflectedVelocity = Vector3.Reflect(velocity, wallNormal);
-        velocity = reflectedVelocity.normalized * velocity.magnitude;
+        if (other.GetInstanceID() < this.GetInstanceID())
+            return;
 
-        transform.position += wallNormal * 0.01f;
+        Vector3 pos1 = transform.position;
+        Vector3 pos2 = other.transform.position;
 
-        Debug.Log("After collision: " + velocity);
+        Vector3 vel1 = velocity;
+        Vector3 vel2 = other.velocity;
+
+        float r1 = ballRadius;
+        float r2 = other.ballRadius;
+
+        float m1 = ballMass;
+        float m2 = other.ballMass;
+
+        Vector3 delta = pos2 - pos1;
+        float distance = delta.magnitude;
+
+        if (distance == 0)
+            return;
+
+        Vector3 normal = delta / distance;
+        Vector3 tangent = new Vector3(-normal.z, 0, normal.x);
+
+        float v1n = Vector3.Dot(vel1, normal);
+        float v1t = Vector3.Dot(vel1, tangent);
+
+        float v2n = Vector3.Dot(vel2, normal);
+        float v2t = Vector3.Dot(vel2, tangent);
+
+        float v1nAfter = (v1n * (m1 - m2) + 2 * m2 * v2n) / (m1 + m2);
+        float v2nAfter = (v2n * (m2 - m1) + 2 * m1 * v1n) / (m1 + m2);
+
+        Vector3 v1nAfterVec = normal * v1nAfter;
+        Vector3 v1tAfterVec = tangent * v1t;
+
+        Vector3 v2nAfterVec = normal * v2nAfter;
+        Vector3 v2tAfterVec = tangent * v2t;
+
+        velocity = v1nAfterVec + v1tAfterVec;
+        other.velocity = v2nAfterVec + v2tAfterVec;
     }
 
     private void HandleWallCollision(Collider collision)
